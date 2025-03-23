78387e8b7171b039657ea8c6b5720a739b50ef55
Benjamin-Dev-HH <ben1100@hotmail.de>
2025-03-23 16:50:15 +0100

    migrations setup

--- backend/fitness_tracker/fitness_tracker/settings.py
+++ backend/fitness_tracker/fitness_tracker/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.users',
-    'apps.workouts',
+    'users',
+    'workouts',
 ]
 
 MIDDLEWARE = [
